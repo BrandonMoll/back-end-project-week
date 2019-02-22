e56365b13a86ac4484f5f9b91a24d1a09e96aa72
BrandonMoll <brandonmoll85@gmail.com>
2019-02-21 20:15:29 -0600

    added user endpoints

--- routers/users_router.js
+++ routers/users_router.js
@@ -8,20 +8,40 @@ const db = knex(dbConfig.development);
 
 router.use(express.json());
 
-router.get('/users/:name', (req, res) => {
-    const {name} = req.params;
-    
-})
-
-router.get('/:id', (req, res) => {
-    const {id} = req.params;
-    db('notes').where('id', id)
+router.get('/', (req, res) => {
+    db('users')
     .then(rows => {
         res.json(rows)
     })
     .catch(err => {
-        res.status(500).json({error: 'error getting note by ID'})
+        res.status(500).json({ message: 'error getting users'})
     })
+});
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    db('users').where('id', id)
+    .then(rows => {
+        res.json(rows)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'error getting user by ID'})
+    })
+});
+
+router.post('/', (req, res) => {
+    const newUser = req.body;
+    if(newUser.user) {
+        db('user').insert(newUser)
+        .then(ids => {
+            res.status(201).json(ids)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'error creating new user'})
+        })
+    } else {
+        res.status(400).json({error: 'Missing user name'})
+    }
 });
 
 module.exports = router
